0b47cdf29a061e789e9c7e5ff688bac4f8288cc9
Herman Polloni <hpolloni@gmail.com>
2022-12-11 19:51:41 -0800

    minimal hilighting

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use std::borrow::Cow::{self, Borrowed, Owned};
-use std::{process::exit, fmt::Display};
-use env_logger::{Env, Builder};
+use env_logger::{Builder, Env};
 use log::debug;
-use rustyline::config::Configurer;
 use rustyline::error::ReadlineError;
-use sqlx::{SqliteConnection, Connection, Row, sqlite::SqliteRow, Column};
-
-use rustyline::completion::FilenameCompleter;
-use rustyline::highlight::{Highlighter, MatchingBracketHighlighter};
 use rustyline::hint::HistoryHinter;
-use rustyline::validate::MatchingBracketValidator;
-use rustyline::{Cmd, CompletionType, Config, EditMode, Editor, KeyEvent};
+use sqlx::{sqlite::SqliteRow, Column, Connection, Row, SqliteConnection};
+use std::borrow::Cow::{self, Borrowed, Owned};
+use std::{fmt::Display, process::exit};
+
+use rustyline::highlight::{Highlighter, MatchingBracketHighlighter};
+use rustyline::{CompletionType, Config, EditMode, Editor};
 use rustyline_derive::{Completer, Helper, Hinter, Validator};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -46,25 +43,32 @@ fn handle_command(line: String) {
 #[derive(Default)]
 struct ResultSet {
     headers: Vec<String>,
-    rows: Vec<Vec<String>>
+    rows: Vec<Vec<String>>,
 }
 
 impl From<Vec<SqliteRow>> for ResultSet {
     fn from(rows: Vec<SqliteRow>) -> Self {
         if rows.is_empty() {
-            return Self::default()
+            return Self::default();
         }
-        let result_set_rows = rows.iter().map(|sql_row| {
-            let mut row = vec![];
-            for i in 0..sql_row.columns().len() {
-                row.push(sql_row.get(i));
-            }
-            row
-        }).collect();
+        let result_set_rows = rows
+            .iter()
+            .map(|sql_row| {
+                let mut row = vec![];
+                for i in 0..sql_row.columns().len() {
+                    row.push(sql_row.get(i));
+                }
+                row
+            })
+            .collect();
 
         Self {
-            headers: rows[0].columns().iter().map(|col| { col.name().to_owned() }).collect(),
-            rows: result_set_rows
+            headers: rows[0]
+                .columns()
+                .iter()
+                .map(|col| col.name().to_owned())
+                .collect(),
+            rows: result_set_rows,
         }
     }
 }
@@ -72,28 +76,32 @@ impl From<Vec<SqliteRow>> for ResultSet {
 impl Display for ResultSet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut builder = tabled::builder::Builder::default();
-    
+
         builder.set_columns(&self.headers);
         for row in self.rows.iter() {
             builder.add_record(row);
         }
-    
+
         let table = builder.build().with(tabled::Style::psql());
 
         write!(f, "{}", table)
     }
 }
 
-
 #[derive(Helper, Completer, Hinter, Validator)]
-struct MyHelper {
+struct LineHelper {
     highlighter: MatchingBracketHighlighter,
     #[rustyline(Hinter)]
     hinter: HistoryHinter,
     colored_prompt: String,
 }
 
-impl Highlighter for MyHelper {
+impl LineHelper {
+    const KEYWORDS : [&str; 6] = ["SELECT", "FROM", "WHERE", "select", "from", "where"];
+}
+
+impl Highlighter for LineHelper {
+
     fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
         &'s self,
         prompt: &'p str,
@@ -107,18 +115,22 @@ impl Highlighter for MyHelper {
     }
 
     fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
-        Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+        Owned("\x1b[2m".to_owned() + hint + "\x1b[m")
     }
 
-    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
-        self.highlighter.highlight(line, pos)
+    fn highlight<'l>(&self, line: &'l str, _pos : usize) -> Cow<'l, str> {
+        let mut lighted = line.to_owned();
+        for keyword in LineHelper::KEYWORDS {
+            lighted = lighted.replace(keyword, &format!("\x1b[1;34m{keyword}\x1b[0m"));
+        }
+
+        Owned(lighted)
     }
 
     fn highlight_char(&self, line: &str, pos: usize) -> bool {
         self.highlighter.highlight_char(line, pos)
     }
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -127,19 +139,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     initialize_logger();
     show_welcome_msg();
 
-     let config = Config::builder()
-        .history_ignore_space(true)
-        .completion_type(CompletionType::List)
-        .edit_mode(EditMode::Vi)
-        .build();
-
-    let h = MyHelper {
+    let h = LineHelper {
         highlighter: MatchingBracketHighlighter::new(),
         hinter: HistoryHinter {},
         colored_prompt: "".to_owned(),
     };
 
-    let mut rl = Editor::with_config(config)?;
+    let mut rl = Editor::new()?;
     rl.set_helper(Some(h));
     if rl.load_history("history.txt").is_err() {
         println!("No previous history.");
@@ -163,8 +169,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                     // TODO: sqlx permits streaming result sets. So we could do
                     // asynchronous fetching of results
-                    let rows = sqlx::query(&query)
-                        .fetch_all(&mut conn).await;
+                    let rows = sqlx::query(&query).fetch_all(&mut conn).await;
                     if let Ok(rows) = rows {
                         let result_set = ResultSet::from(rows);
 
@@ -172,17 +177,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                         println!("executed successfully. ");
                     }
+
                     buffer = vec![];
                     prompt = default_prompt;
                 } else {
                     buffer.push(line);
-                    prompt = "|  "; 
+                    prompt = "|  ";
                 }
-            },
+            }
             Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
                 debug!("Interrupted!");
                 break;
-            },
+            }
             Err(err) => {
                 println!("Unexpected error while reading input: {:?}", err);
                 break;
@@ -192,4 +198,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     rl.append_history("history.txt")?;
     Ok(())
 }
-
